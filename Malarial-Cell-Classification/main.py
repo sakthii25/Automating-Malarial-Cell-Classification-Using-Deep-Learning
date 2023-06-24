33fc7a2fb244383ad2294545a0679c1fd2f5df89
Sakthi Narayanan R <106539713+sakthii25@users.noreply.github.com>
2023-06-24 09:53:12 +0530

    Update main.py

--- Malarial-Cell-Classification/main.py
+++ Malarial-Cell-Classification/main.py
@@ -1,53 +1,15 @@
 import streamlit as st
-import numpy as np
-import cv2
-from keras.models import load_model
-from pdf import generate_pdf
-import base64
+from single_image_predictor import predict_single_image
+from multiple_images_predictor import predict_multiple_images
 
-model = load_model("E:\projects\Malarial-Cell-Classification\model.h5")
+# Page navigation
+choice = st.sidebar.selectbox('MCC', ['Home', 'Single image classifier', 'Multiple image classifier'])
 
-st.title("Malarial Cell Classification")
+if choice == "Home":
+    st.title("Malarial Cell Classifier")
 
-c1,c2 = st.columns(2)
+elif choice == "Single image classifier":
+    predict_single_image()
 
-name = c1.text_input("Enter name ")
-age = c2.text_input("Enter age ")
-gender = c1.selectbox("Select your gender", ["Male", "Female", "Other"])
-date = c2.date_input("Enter date ")
-file = st.file_uploader("Choose an image",type=["jpg","jpeg","png"])
-
-if file is not None:
-    x = []
-    data = file.read()
-    arr = np.frombuffer(data,np.uint8)
-    img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-    img = cv2.resize(img,(150,150))
-    x.append(img)
-    x = np.array(x)
-    res = model.predict(x)
-    y = round(res[0,0])
-    if(y == 1):
-        test_result = "The given cell of the person is infected by malaria."
-    else:
-        test_result = "The given cell of the person is not infected by malaria."
-    if st.button("Generate PDF"):
-        if(not name or not age):
-            st.error("Please fill all the details.")
-        else:
-            pdf = generate_pdf(name, age, gender,date,test_result)
-            pdf_output = pdf.output(dest="S").encode("latin-1")
-
-            # Display PDF
-            st.header(f"Displaying PDF: {name}'s Test Report")
-
-            # Convert PDF data to base64 for embedding
-            pdf_base64 = base64.b64encode(pdf_output).decode("latin-1")
-            pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" frameborder="0"></iframe>'
-            st.write(pdf_display, unsafe_allow_html=True)
-
-            # Download PDF
-            href = f'<a href="data:application/pdf;base64,{pdf_base64}" download="output.pdf">Download PDF</a>'
-            st.markdown(href, unsafe_allow_html=True)
-
-
+else:
+    predict_multiple_images()
